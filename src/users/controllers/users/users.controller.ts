bbb919f4ea05e3c6d430a093ada5a7d1ac24cbf8
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-13 16:57:57 +0600

    post request has been implemented

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    getUsers('/') {
+    // get all users data routes
+    @Get('/index')
+    getUsers() {
         return { username: 'imhayatunnabi', email: 'imhayatunnabi@gmail.com' }
     }
-    
+    //post a user to create a users
+    @Post('/store')
+    createUser(@Req() request: Request, @Res() response: Response) {
+        console.log(request.body);
+        response.send('User has been created');
+    }
+
+
 }
